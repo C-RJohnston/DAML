c14f9e23e3b9bdd12aec71311510f9d3c1e84767
Cameron <cameron.robin.johnston@gmail.com>
2022-03-11 00:26:28 +0000

    increased data

--- Sem 2/Project 3/include/Consts.h
+++ Sem 2/Project 3/include/Consts.h
@@ -5,7 +5,7 @@
 #include "G4SystemOfUnits.hh"
 #include "tls.hh"
 
-static const G4double radius = 75*cm;
-static const G4double detectorRingWidth = 2.0*cm;
+static const G4double radius = 100*cm;
+static const G4double detectorRingWidth = 1.0*cm;
 
 #endif
